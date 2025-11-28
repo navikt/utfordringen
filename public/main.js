96cb6f34b061607ab285f879537ec66c69da3565
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-11-28 09:03:53 +0100

    Prøve på noe totale poeng greier

--- public/main.js
+++ public/main.js
@@ -32,24 +32,68 @@ const utfordringer = [
                 "poeng": 7
             }
         ]
-    }
+    },
+    {
+        "tittel": "Ping pong",
+        "poenger": [
+            {
+                "deltaker": "Pinnsvinn",
+                "poeng": 9
+            },
+            {
+                "deltaker": "Hassan",
+                "poeng": 8
+            },
+        ]
+    },
+    {
+        "tittel": "Golf",
+        "poenger": [
+            {
+                "deltaker": "Pinnsvinn",
+                "poeng": 0
+            },
+            {
+                "deltaker": "Hassan",
+                "poeng": 3
+            },
+            {
+                "deltaker": "Steinar",
+                "poeng": 6
+            }
+        ]
+    },
 ]
 
 // TODO: deltaker med høyest score får gul trøye
-const lagTotalScoreObjekt = () => {
+const lagSortertListeOverDeltakersTotalePoeng = () => {
     const listeOverTotalScores = []
+    const copy = [...utfordringer]
+    copy.map((utfordring) => {
+        // 1. Finne om navn matcher i listen over total scores, hvis ja, oppdater objektet og legg til score
+        // 2. Hvis ikke legg til i listen over total scores
+        utfordring.poenger.map((poeng) => {
+            const delatkerFunnet = listeOverTotalScores.find(element => element.deltaker === poeng.deltaker)
 
-    utfordringer.map((utfordring, index) => {
-        if (index === 0) {
-            utfordring.poenger.map(poeng => {
+            if (delatkerFunnet) {
+                const deltakerFunnetIndex = listeOverTotalScores.findIndex(element => element.deltaker === poeng.deltaker)
+
+                const oppdaterPoengDeltaker = {
+                    ...delatkerFunnet,
+                    poeng: delatkerFunnet.poeng += poeng.poeng
+                }
+
+                listeOverTotalScores[deltakerFunnetIndex] = oppdaterPoengDeltaker
+            } else {
                 listeOverTotalScores.push(poeng)
-            })
-        } else {
-            // 1. Finne om navn matcher i listen over total scores, hvis ja, oppdater objektet og legg til score
-            // 2. Hvis ikke legg til i listen over total scores
-        }
+            }
+        })
     })
+
+    return listeOverTotalScores
 }
+
+console.log(lagSortertListeOverDeltakersTotalePoeng())
 
 const listUtfordringer = () => {
     const rootKomponent = document.getElementById("utfordringer")
